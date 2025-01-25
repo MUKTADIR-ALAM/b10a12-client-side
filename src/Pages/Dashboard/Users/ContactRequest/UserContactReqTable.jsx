03de3130e76eaf9d70eaa9c6331ab84027b6f3a5
MUKTADIR-ALAM <muktadiralam38@gmail.com>
2025-01-25 15:30:16 +0600

    make contact req function

--- src/Pages/Dashboard/Users/ContactRequest/UserContactReqTable.jsx
+++ src/Pages/Dashboard/Users/ContactRequest/UserContactReqTable.jsx
@@ -20,7 +20,31 @@ export default function UserContactReqTable({ item,refetch }) {
     },
   });
 
-  
+  const hadleDelete = async (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        
+        const { data } = await axiosSecure.delete(`/delContactRequest/${id}`);
+        console.log(data);
+        if(data.deletedCount){
+          Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+        }
+        refetch();
+      }
+    });
+  };
 
   if (isPending) {
     return (
